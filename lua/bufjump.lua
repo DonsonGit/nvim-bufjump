b5a3d6df07de1f9488121001001e8bf0844f5794
don <donsongit@163.com>
2021-12-17 18:01:45 +0800

    jump to

--- lua/bufjump.lua
+++ lua/bufjump.lua
@@ -6,6 +6,42 @@ local g, o, wo, bo = vim.g, vim.o, vim.wo, vim.bo
 local fmt = string.format
 local M = {}
 
+function M.jumpTo(name)
+    if type(name) ~= 'string' then return end
+
+    local cur_bufnr = api.nvim_get_current_buf()
+    if not cur_bufnr or cur_bufnr < 0 then return end
+
+    local cur_buf_name = api.nvim_buf_get_name(cur_bufnr)
+
+    local buffers = vim.tbl_filter(
+        function (buf)
+            return api.nvim_buf_is_valid(buf) and
+            bo[buf].buflisted and
+            bo[buf].filetype ~= 'qt' and
+            bo[buf].buftype ~= 'terminal' and
+            string.find(api.nvim_buf_get_name(buf), name, 1, true)
+        end,
+        api.nvim_list_bufs()
+    )
+
+    if #buffers <= 0 then return end
+
+    for _, buf in ipairs(buffers) do
+        if api.nvim_buf_get_name(buf) ~= cur_buf_name then
+            api.nvim_win_set_buf(0, buf)
+            return
+        end
+    end
+end
+
+function M.close_current()
+    local bufnr = api.nvim_get_current_buf()
+    if not bufnr or bufnr < 0 then return end
+
+    cmd(fmt("bd "..bufnr))
+end
+
 function M.close_others()
 
     local cur_bufnr = api.nvim_get_current_buf()
--- plugin/bufjump.vim
+++ plugin/bufjump.vim
@@ -1,7 +1,22 @@
 " nvim-bufjump
 
-if exists('g:loaded_bufjump')
-  finish
+if exists('g:loaded_bufjump') | finish | endif
+
+if !has('nvim-0.5')
+    echohl Error
+    echomsg "bufjump.nvim is only available for Neovim versions 0.5 and above"
+    echohl clear
+    finish
 endif
+
+let s:save_cpo = &cpo
+set cpo&vim
+
+command! -nargs=? BufJumpTo lua require'bufjump'.jumpTo(<q-args>)
+command! BufCloseOthers lua require'bufjump'.close_others()
+command! BufCloseCur lua require'bufjump'.close_current()
+
+let &cpo = s:save_cpo
+unlet s:save_cpo
 
 let g:loaded_bufjump = 1
